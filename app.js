8da9e2d3f3b3724e0ac4302585b532ac1c8986a6
Alepsis <juanrosales.d@gmail.com>
2020-07-01 11:54:24 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+// Requires 
+var express = require('express');
+var mongoose = require('mongoose');
+
+
+
+
+
+// Iniciarlizar variables
+
+var app = express();
+
+// Conexion a la base de datos
+mongoose.connection.openUri('mongodb://localhost:27017/montesoridb', function(err, res) {
+    if (err) throw err;
+
+    console.log('Base de Datos: \x1b[32m%s\x1b[0m ', 'online');
+
+
+});
+
+// Rutas 
+app.get('/', function(req, res, next) {
+
+    res.status(200).json({
+        ok: true,
+        mensaje: 'Peticion realizada correctamente'
+    })
+
+});
+
+
+
+
+// Escuchar peticiones 
+app.listen(3000, function() {
+    console.log('Express server puerto 3000: \x1b[32m%s\x1b[0m ', 'online');
+});
