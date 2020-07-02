57d970e7738aa4ca3e641d2cb0362bbb07c78a40
Alepsis <juanrosales.d@gmail.com>
2020-07-02 16:32:02 -0500

    Usuario y token listo

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Requires 
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 
 
@@ -9,6 +10,13 @@ var mongoose = require('mongoose');
 // Iniciarlizar variables
 
 var app = express();
+
+
+// Body Parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 
 // Conexion a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/montesoridb', function(err, res) {
@@ -19,17 +27,16 @@ mongoose.connection.openUri('mongodb://localhost:27017/montesoridb', function(er
 
 });
 
-// Rutas 
-app.get('/', function(req, res, next) {
+//  Importar Rutas 
 
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Peticion realizada correctamente'
-    })
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 
-});
-
-
+// rutas
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
 
 // Escuchar peticiones 
---
+++ config/config.js
@@ -0,0 +1,1 @@
+module.exports.SEED = '@esmi-seed-de-montesori';
